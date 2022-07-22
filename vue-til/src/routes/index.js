51179840d0e9bba38bdc59f818e7f74e3210f1c6
Jess <nakedthunder00@gmail.com>
2022-07-22 22:49:41 +0900

    code spliting

--- vue-til/src/routes/index.js
+++ vue-til/src/routes/index.js
@@ -9,7 +9,13 @@ Vue.use(VueRouter);
 //인스턴스 export default 라우터의 인스턴스가 생성되면서
 //라우팅 인스턴스가 밖으로 나가게하는게 export의 역할
 export default new VueRouter({
+  mode: 'history', //url의 #이 제거된다.
   routes: [
+    //기본루트: 로그인
+    {
+      path: '/',
+      redirect: '/login',
+    },
     {
       path: '/login',
       // component: LoginPage, "코드스플리팅"
@@ -18,6 +24,11 @@ export default new VueRouter({
     {
       path: '/signup',
       component: () => import('@/views/SignupPage.vue'),
+    },
+    // 없는 페이지 나머지의 모든 url반응
+    {
+      path: '*',
+      component: () => import('@/views/NotFoundPage.vue'),
     },
   ],
 });
