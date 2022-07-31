eb2279985290e6b26b0d7e12918bc5713d9902cc
Jess <nakedthunder00@gmail.com>
2022-07-31 14:05:53 +0900

    create baseURL and markup loginPage

--- vue-til/src/api/index.js
+++ vue-til/src/api/index.js
@@ -5,11 +5,15 @@ const instance = axios.create({
   baseURL: process.env.VUE_APP_API_URL,
 });
 
-//url주소로 axios.post가 날라간다. export한 것을 호출할 수 있다.
+//회원가입
 function registerUser(userData) {
   //return: axios의 결과가 promise여서 return 해줘야 비동기 동작을 수행할 수 있음
   //userData: userData를 함수인자로 받아서 post로 같이 넘긴다.
   return instance.post('signup', userData);
 }
+//로그인
+function loginUser(userData) {
+  return instance.post('login', userData);
+}
 
-export { registerUser };
+export { registerUser, loginUser };
---
+++ vue-til/src/components/LoginForm.vue
@@ -0,0 +1,41 @@
+<template>
+  <form @submit.prevent="submitForm">
+    <div>
+      <label for="username">id: </label>
+      <input type="text" id="username" v-model="username" />
+    </div>
+    <div>
+      <label for="password">pw: </label>
+      <input type="text" id="password" v-model="password" />
+    </div>
+    <button type="submit">로그인</button>
+  </form>
+</template>
+
+<script>
+import axios from 'axios';
+import { loginUser } from '@/api/index';
+export default {
+  data() {
+    return {
+      //input이 필요함 v-model연동
+      username: '',
+      password: '',
+    };
+  },
+  methods: {
+    submitForm() {
+      const userData = {
+        username: this.username,
+        password: this.password,
+      };
+
+      //여기서 axios.post는 뭘까
+      //1. userData객체를 넘기고 loginUser만듬 api에 호출을 해야한느데
+      axios.post();
+    },
+  },
+};
+</script>
+
+<style></style>
