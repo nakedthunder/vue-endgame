dd05f5fa3c1d52cca110c31853444b8d64ad349d
Jess <nakedthunder00@gmail.com>
2022-08-02 22:59:17 +0900

    add validate email and check empty pwd

--- vue-til/src/components/LoginForm.vue
+++ vue-til/src/components/LoginForm.vue
@@ -8,13 +8,16 @@
       <label for="password">pw: </label>
       <input type="text" id="password" v-model="password" />
     </div>
-    <button type="submit">로그인</button>
+    <button :disabled="!isUsernameValid || !password" type="submit">
+      로그인
+    </button>
     <p>{{ logMessage }}</p>
   </form>
 </template>
 
 <script>
 import { loginUser } from '@/api/index';
+import { validateEmail } from '@/utils/validation';
 export default {
   data() {
     return {
@@ -24,17 +27,33 @@ export default {
       logMessage: '',
     };
   },
+  computed: {
+    isUsernameValid() {
+      return validateEmail(this.username);
+    },
+  },
   methods: {
     async submitForm() {
-      const userData = {
-        username: this.username,
-        password: this.password,
-      };
+      try {
+        //필요한 로직
+        const userData = {
+          username: this.username,
+          password: this.password,
+        };
 
-      const { data } = await loginUser(userData);
-      console.log(data.user.username);
-      this.logMessage = `${data.user.username} 님 환영합니다.`;
-      this.initForm();
+        const { data } = await loginUser(userData);
+        console.log(data.user.username);
+        this.logMessage = `${data.user.username} 님 환영합니다.`;
+        this.initForm();
+      } catch (error) {
+        //에러 핸들링한 코드
+        //Network > response
+        // console.log(error.ressponse); 에러로그 메세지를 담아 노출
+        this.logMessage = error.response.data;
+      } finally {
+        //사용하는 곳이 중복된다면
+        this.initForm();
+      }
     },
     initForm() {
       this.username = '';
